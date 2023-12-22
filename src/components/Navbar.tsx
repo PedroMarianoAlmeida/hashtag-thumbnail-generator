061bf3e6ecc26a050b6be35983d3a104f93f3d81
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-12-21 21:58:31 -0500

    feat: create Navbar base

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center">
+      <Link className="flex items-center justify-center" href="/">
+        <Image src="/logo.png" width={50} height={50} alt="logo" />
+        <span>SEO Helper</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          Dashboard / Login
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="#"
+        >
+          About
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
