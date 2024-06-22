7d90e8ce38b44d93b3c9e2007babe097b424e405
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-06-22 14:33:15 -0400

    get user usage working

--- src/server/actions/userCount.ts
+++ src/server/actions/userCount.ts
@@ -1,32 +1,40 @@
 "use server";
 
-import { get, ref } from "firebase/database";
+import { get, ref, type DataSnapshot } from "firebase/database";
 import { asyncWrapper } from "@/utils/asyncWrapper";
 import { areInTheSameDay } from "@/utils/dates";
 import { PrismaClient } from "@prisma/client";
-import firebase from "firebase/compat/app";
 import { database } from "../../../firebaseConfig";
+
 const prisma = new PrismaClient();
+
+const sanitizeUserCount = (
+  user: DataSnapshot
+): { lastUsage: number; dailyUsage: number } => {
+  const lastUsage = user.child("lastUsage").val();
+  const dailyUsage = user.child("dailyUsage").val();
+
+  if (typeof lastUsage !== "number") throw new Error();
+  if (typeof dailyUsage !== "number") throw new Error();
+  console.log({ lastUsage, dailyUsage });
+  return { lastUsage, dailyUsage };
+};
 
 export const getUserCountUsageForToday = async (email: string) => {
   return asyncWrapper(async () => {
-    const testRef = ref(database, "test");
-    const testValue = await get(testRef);
-
-    const userCountUsage = 3;
-
-    if (!userCountUsage) {
+    const userRef = ref(database, `users/${btoa(email)}`);
+    const existentUser = await get(userRef);
+    if (!existentUser) {
       return 0;
     }
 
-    // const { lastUsage, dailyUsage } = userCountUsage;
+    const { dailyUsage, lastUsage } = sanitizeUserCount(existentUser);
 
-    // if (lastUsage && !areInTheSameDay(lastUsage, new Date())) {
-    //   return 0;
-    // }
+    if (lastUsage && !areInTheSameDay(new Date(lastUsage), new Date())) {
+      return 0;
+    }
 
-    // return dailyUsage;
-    return testValue;
+    return dailyUsage;
   });
 };
 
